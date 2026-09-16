18a8c6efe0e9a668f51b35954d1e4c43bbc2b0af
Subathra_C <subathrachidambaram07@gmail.com>
2026-09-16 22:43:49 +0530

    Added buffered database connection

--- db.py
+++ db.py
@@ -4,7 +4,8 @@ db = mysql.connector.connect(
     host="localhost",
     user="root",
     password="Suba_pr0",
-    database="farmers_market"
+    database="farmers_market",
+    buffered=True
 )
 
 print("Database Connected Successfully")
